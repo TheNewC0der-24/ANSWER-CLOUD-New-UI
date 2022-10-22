815f721aead5967c797675db715b17011e6200cb
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-23 00:56:15 +0530

    Create application Context

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 import LoadingScreen from '../Components/LoadingScreen';
 
-const Context = ({ children }) => {
+const Context = createContext();
 
-    const [loading, setLoading] = useState(true);
+const ContextProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(false);
+
+    const initialize = async () => {
+        try {
+            setLoading(true);
+        } catch (error) {
+            console.log(error);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        initialize();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const values = {
+        login,
+        logout,
+    }
 
     return (
-        <div>
+        <Context.Provider value={values}>
             {loading ? <LoadingScreen /> : children}
-        </div>
+        </Context.Provider>
     )
 }
 
-export default Context;
+
+export default { Context, ContextProvider };
