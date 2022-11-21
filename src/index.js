ad0827d9d771272385149a6e2c330219d195843f
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-21 05:58:43 +0530

    Add Localization Provider and Adapter Date Fns

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './Context/Context';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode >
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </LocalizationProvider>
+  </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
