2aae1f5e1fd2038e326f262527d2f73aca1e69c9
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-21 05:48:13 +0530

    Create Dashboard

---
+++ src/Components/Dashboard/Chart.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+    Box,
+    Typography,
+    Card,
+    CardContent,
+} from '@mui/material';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const options = {
+    indexAxis: 'y',
+    elements: {
+        bar: {
+            borderWidth: 2,
+        },
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            display: true,
+            position: 'top',
+        },
+        title: {
+            display: false,
+        },
+    },
+    scales: {
+        x: {
+            ticks: {
+                beginAtZero: true,
+                callback: function (value) {
+                    return value + '%';
+                }
+            }
+        }
+    },
+};
+
+const labels = [
+    'On-the job, hands-on or experiential learning',
+    'Mentoring or coaching',
+    'Instructor-led or classroom training',
+    'Formal curriculum or courses*',
+    'Videos',
+    'Online simulations games',
+    'Virtual instructor-let training**',
+    'eLearning courses',
+    'Podcast',
+    'Augmented reality',
+    'Virtual reality',
+    'Social collaboration or learning applications***',
+    'Mobile device notifications or text messages',
+];
+
+const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Share of respondents in (%)',
+            data: [90, 83, 78, 60, 57, 54, 52, 44, 35, 30, 29, 23, 20],
+            borderColor: 'rgb(25, 118, 210)',
+            backgroundColor: 'rgba(25, 118, 210, 0.5)',
+        },
+    ],
+};
+
+
+const Chart = () => {
+    return (
+        <>
+            <Box>
+                <Typography sx={{ fontSize: { xs: "18px", sm: "20px", md: "22px" } }} variant="h5">
+                    Share of employees worldwide who believe learning technologies are effective in 2018, by type of technology
+                </Typography>
+                <Box mt={1}>
+                    <Card elevation={2}>
+                        <CardContent>
+                            <Bar options={options} data={data} />
+                        </CardContent>
+                    </Card>
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default Chart;
--- src/Components/Dashboard/index.jsx
+++ src/Components/Dashboard/index.jsx
@@ -1,8 +1,93 @@
 import React from 'react';
+import {
+    Box,
+    Typography,
+    Avatar,
+    Grid,
+    Card,
+    CardActionArea,
+    CardContent,
+    Divider,
+} from "@mui/material";
+
+import styles from './Dashboard.module.css';
+
+// Icon
+import { Icon } from "@iconify/react";
+import Chart from './Chart';
+import ViewInteraction from './ViewInteraction';
+
 
 function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <>
+            <Box>
+                <Typography sx={{ display: "flex", alignItems: "center" }} variant="h5">
+                    <Avatar variant="rounded" className={styles.avatar}>
+                        <Icon
+                            icon="ic:sharp-dashboard-customize"
+                            className={styles.dashboardIcon}
+                        />
+                    </Avatar>
+                    Dashboard
+                </Typography>
+
+                <Grid mt={2} container spacing={2}>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ backgroundColor: "#C1F0F5" }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant="subtitle1" component="div">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, iste porro! Voluptatum ex natus beatae eligendi mollitia quos, numquam assumenda.
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ backgroundColor: "#DFD8FD" }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant="subtitle1" component="div">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, iste porro! Voluptatum ex natus beatae eligendi mollitia quos, numquam assumenda.
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ backgroundColor: "#FFE2BD" }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant="subtitle1" component="div">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, iste porro! Voluptatum ex natus beatae eligendi mollitia quos, numquam assumenda.
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <Card sx={{ backgroundColor: "#FFD2CC" }}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography variant="subtitle1" component="div">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, iste porro! Voluptatum ex natus beatae eligendi mollitia quos, numquam assumenda.
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                </Grid>
+                <Divider sx={{ mt: 3, mb: 2 }} />
+                {/******************** Chart ********************/}
+                <Chart />
+
+                {/******************** View Interaction ********************/}
+                <Box mt={2}>
+                    <ViewInteraction />
+                </Box>
+            </Box>
+        </>
     )
 }
 
