a10d6bd93471905d0e767e07e71104aee0835728
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-12-02 00:02:55 +0530

    🐞 fix: form routing

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -75,7 +75,6 @@ const Router = () => {
         },
         {
             path: "form",
-            element: <Welcome />,
             children: [
                 {
                     path: "",
