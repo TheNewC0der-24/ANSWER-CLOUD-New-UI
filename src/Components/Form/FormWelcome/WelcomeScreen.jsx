e91074b5ba9d5390fcc1e979bcd171a3fba1ad10
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-12-08 03:24:19 +0530

    Add routing to work page

--- src/Components/Form/FormWelcome/WelcomeScreen.jsx
+++ src/Components/Form/FormWelcome/WelcomeScreen.jsx
@@ -5,25 +5,31 @@ import {
     Paper,
     Stack,
     Typography,
-    Button
+    Button,
+    Card,
+    CardContent,
+    Container
 } from '@mui/material';
 
 const WelcomeScreen = () => {
     const navigate = useNavigate();
 
-    const handleNext = () => {
-        // navigate
-    };
-
     return (
         <>
+            <Container maxWidth="lg">
+                <Card component={Stack} p={2} sx={{ backgroundColor: "#ffc106" }}>
+                    <CardContent>
+
+                    </CardContent>
+                </Card>
+            </Container>
+
             <Paper component={Stack} p={3} direction={{ xs: 'column', sm: 'row', md: "row" }}>
-                {/*************** Internal Bot ***************/}
                 <Stack display={{ xs: 'none', sm: 'none', md: "block" }} width={{ xs: '100%', sm: '100%', md: "40%" }}>A</Stack>
                 <Stack display={{ xs: 'none', sm: 'none', md: "block" }} width={{ xs: '100%', sm: '100%', md: "60%" }}>C</Stack>
                 <Typography variant="h4">Welcome</Typography>
                 <Typography mb={2} variant="h6">This interaction is designed to provide you a simulated experience.</Typography>
-                <Button variant="contained" size='small' onClick={handleNext}>Next</Button>
+                <Button variant="contained" size='small' onClick={() => navigate('/form/work')}>Next</Button>
             </Paper>
         </>
     )
