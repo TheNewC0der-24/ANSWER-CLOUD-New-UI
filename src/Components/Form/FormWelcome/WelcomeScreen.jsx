5b96ad427e4ca356ac4402b54a8ceee9bc7a5b12
ayushjain27 <ayushjain201574@gmail.com>
2022-12-08 06:06:51 +0530

    WelcomeSceen Changes

--- src/Components/Form/FormWelcome/WelcomeScreen.jsx
+++ src/Components/Form/FormWelcome/WelcomeScreen.jsx
@@ -20,9 +20,9 @@ const WelcomeScreen = () => {
         <>
             <Container maxWidth="lg">
                 <Card component={Stack} p={2} sx={{ backgroundColor: "#ffc106" }}>
-                    <CardContent display={{ xs: 'flex', sm: 'flex', md: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
-                        <Stack display={{ xs: 'none', sm: 'none', md: "block" }} width={{ xs: '100%', sm: '100%', md: "40%" }}><img src={loginImg} alt="login" style={{ width: '100%', height: '100%' }} /></Stack>
-                        <Stack display={{ xs: 'none', sm: 'none', md: "block" }} width={{ xs: '100%', sm: '100%', md: "60%" }}><Typography variant="h4">Welcome</Typography>
+                    <CardContent sx={{ display: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
+                        <Stack p={2} display={{ xs: 'none', sm: 'none', md: "block" }}><img src={loginImg} alt="login" style={{ width: '100%', height: '100%' }} /></Stack>
+                        <Stack p={6}><Typography variant="h4">Welcome</Typography>
                             <Typography mb={2} variant="h6">This interaction is designed to provide you a simulated experience.</Typography>
                             <Button variant="contained" size='small' onClick={() => navigate('/form/work')}>Next</Button></Stack>
                     </CardContent>
