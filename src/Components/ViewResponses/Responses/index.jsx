a2a09b00391dd446138e07679f31090f7cec54e1
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-12-09 22:27:12 +0530

    Create Responses page

--- src/Components/ViewResponses/Responses/index.jsx
+++ src/Components/ViewResponses/Responses/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-// import { Video, ReactAudio } from "reactjs-media";
+import styles from './Responses.module.css';
 
 import {
     Container,
     Box,
+    Alert,
     Card,
     CardContent,
     Typography,
@@ -58,7 +59,7 @@ const Responses = () => {
                 <Box mt={5}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={12} md={6} lg={4}>
-                            <Card sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
+                            <Card className={styles.card} sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
                                 <CardContent>
                                     <Typography mb={2} variant="h5">Question-1</Typography>
                                     <Box>
@@ -81,14 +82,12 @@ const Responses = () => {
                                         </DialogTitle>
                                         <DialogContent>
                                             <DialogContentText id="alert-dialog-description">
-                                                {/* <Video
-                                                    src={video}
-                                                    primaryColor="#1976d2"
-                                                /> */}
+                                                <video controls>
+                                                    <source src={video} type="video/mp4" />
+                                                </video>
                                             </DialogContentText>
                                         </DialogContent>
                                         <DialogActions>
-                                            {/* <Button onClick={handleClose}>Disagree</Button> */}
                                             <Button onClick={handleClose} autoFocus>
                                                 Close
                                             </Button>
@@ -98,7 +97,7 @@ const Responses = () => {
                             </Card>
                         </Grid>
                         <Grid item xs={12} sm={12} md={6} lg={4}>
-                            <Card sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
+                            <Card className={styles.card} sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
                                 <CardContent>
                                     <Typography mb={2} variant="h5">Question-1</Typography>
                                     <Box>
@@ -121,10 +120,9 @@ const Responses = () => {
                                         </DialogTitle>
                                         <DialogContent>
                                             <DialogContentText id="alert-dialog-description">
-                                                {/* <ReactAudio
-                                                    src={audio}
-                                                    poster="/poster.png"
-                                                /> */}
+                                                <audio controls>
+                                                    <source src={audio} type="audio/mpeg" />
+                                                </audio>
                                             </DialogContentText>
                                         </DialogContent>
                                         <DialogActions>
@@ -137,10 +135,18 @@ const Responses = () => {
                             </Card>
                         </Grid>
                         <Grid item xs={12} sm={12} md={6} lg={4}>
-                            <Card sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
+                            <Card className={styles.card} sx={{ backgroundColor: "#f6f6f6", borderTop: '5px solid #1976d2' }}>
                                 <CardContent>
                                     <Typography mb={2} variant="h5">Question-1</Typography>
+                                    <Box>
+                                        <Typography variant="subtitle1">What is axios?</Typography>
+                                    </Box>
                                     <Divider />
+                                    <Box mt={2}>
+                                        <Alert severity="success">
+                                            Option A
+                                        </Alert>
+                                    </Box>
                                 </CardContent>
                             </Card>
                         </Grid>
