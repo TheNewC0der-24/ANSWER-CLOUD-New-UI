7d0bb4f963f2f423df711b067edecf20b9a0bfa7
ayushjain27 <ayushjain201574@gmail.com>
2022-12-11 10:32:08 +0530

    new Changes

--- src/Components/Form/FormQuestion/index.jsx
+++ src/Components/Form/FormQuestion/index.jsx
@@ -94,62 +94,65 @@ const FormQuestion = () => {
 
     return (
         <>
-            <Container maxWidth="lg">
-                <Card component={Stack} p={2} sx={{ backgroundColor: "#ffc106" }}>
-                    <CardContent sx={{ display: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
-                        <Stack p={2} sx={{ display: "block", width: '100%' }}>
-                            <Typography variant="h6">1/4</Typography>
-                            <Typography variant="h6">Tell me about yourself ?</Typography>
-                            <Typography mb={2} variant="h6">Hint : Speak about your educational background, skills, experience, etc.</Typography>
-                            <Divider my={2}></Divider>
-                            <Button my={2} onClick={handleStartRecording} variant="contained" size='small'>{isActive ? "Pause" : "Answer"}</Button>
-                            {
-                                isActive && (
-                                    <Button my={2} onClick={handleStopRecording} variant="contained" size='small'>Save Answer</Button>
-
-                                )
-                            }
-                            <Button my={2} onClick={() => window.location.reload()} variant="contained" size='small'>Retake Answer</Button>
-                            <Divider my={2}></Divider>
-                            <Box my={2} sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                <Button variant="contained" size='small'>Back</Button>
-                                <Button onClick={() => navigate('/form/audio-question')} variant="contained" size='small'>Next</Button>
-                            </Box>
-                        </Stack>
-                    </CardContent>
-                </Card>
-            </Container>
-            <Box mb={3} sx={{ display: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
-                <Card my={2} component={Stack} p={1} sx={{ width: '100%' }}>
-                    <CardContent sx={{ backgroundColor: "#ffc106" }}>
-                        <Typography variant="h4">Video Context</Typography>
-                        <video
-                            // src={video1}
-                            title="YouTube video"
-                            controls
-                            autoPlay
-                        ></video>
-                    </CardContent>
-                </Card>
-                <Card my={2} component={Stack} p={1} sx={{ width: '100%' }}>
-                    <CardContent sx={{ backgroundColor: "#ffc106", display: 'flex', height: '100%' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
-                        <Stack sx={{ width: '100%' }} p={6}><Typography variant="h4">Answer Box</Typography>
-                            <Typography mb={2} variant="h6">time remaining for this question</Typography><Typography variant="h4">{time.m >= 10 ? time.m : "0" + time.m}&nbsp;:&nbsp;{time.s >= 10 ? time.s : "0" + time.s}</Typography>
-                            <Alert severity="error">Answer recording starts after you hit "Answer" button above.</Alert>
-                            {
-                                isActive && (
-                                    <div className="ratio ratio-16x9">
-                                        {status !== "stopped" ? (
-                                            <VideoPreview stream={previewStream} />
-                                        ) : (
-                                            <video src={mediaBlobUrl} controls />
-                                        )}
-                                    </div>
-                                )
-                            }
-                        </Stack>
-                    </CardContent>
-                </Card>
+            <Box gap={2} sx={{ display: 'flex' }} p={2} direction={{ xs: 'column', sm: 'row', md: "row" }} >
+                <Box sx={{ display: 'flex', flexDirection: 'column' }} width={{ xs: '100%', sm: '50%', md: "50%" }}>
+                    <Card component={Stack} p={1} sx={{ backgroundColor: "#F7F8F9" }}>
+                        <CardContent sx={{ display: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
+                            <Stack p={1} sx={{ display: "block", width: '100%' }}>
+                                <Typography variant="h6">1/4</Typography>
+                                <Typography variant="h6">Tell me about yourself ?</Typography>
+                                <Typography mb={2} variant="h6" color='primary'>Hint : Speak about your educational background, skills, experience, etc.</Typography>
+                                <Box mb={1} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Button my={2} onClick={handleStartRecording} variant="contained" color='success' size='small'>{isActive ? "Pause" : "Answer"}</Button>
+                                {
+                                    isActive && (
+                                        <Button my={2} onClick={handleStopRecording} variant="contained" size='small'>Save Answer</Button>
+                                        
+                                        )
+                                    }
+                                <Button my={2} onClick={() => window.location.reload()} variant="contained" size='small'>Retake Answer</Button>
+                                </Box>
+                                <Divider ></Divider>
+                                <Box mt={2} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <Button variant="outlined" size='small'>Back</Button>
+                                    <Button onClick={() => navigate('/form/audio-question')} variant="contained" size='small'>Next</Button>
+                                </Box>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                    <Card my={2} component={Stack} p={1} sx={{ width: '100%', backgroundColor: "#F7F8F9" }}>
+                        <CardContent>
+                            <Typography variant="h4">Video Context</Typography>
+                            <video
+                                // src={video1}
+                                title="YouTube video"
+                                controls
+                                autoPlay
+                            ></video>
+                        </CardContent>
+                    </Card>
+                </Box>
+                <Box sx={{ width: '100%', height: '100%' }}>
+                    <Card component={Stack} p={1} sx={{ backgroundColor: "#F7F8F9", width: '100%' }}>
+                        <CardContent sx={{ display: 'flex' }} direction={{ xs: 'column', sm: 'row', md: "row" }}>
+                            <Stack sx={{ width: '100%' }} p={3}><Typography variant="h4">Answer Box</Typography>
+                                <Typography mb={2} variant="h6">time remaining for this question</Typography><Typography variant="h4">{time.m >= 10 ? time.m : "0" + time.m}&nbsp;:&nbsp;{time.s >= 10 ? time.s : "0" + time.s}</Typography>
+                                <Alert severity="error">Answer recording starts after you hit "Answer" button above.</Alert>
+                                {
+                                    isActive && (
+                                        <div className="ratio ratio-16x9">
+                                            {status !== "stopped" ? (
+                                                <VideoPreview stream={previewStream} />
+                                            ) : (
+                                                <video src={mediaBlobUrl} controls />
+                                            )}
+                                        </div>
+                                    )
+                                }
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Box>
             </Box>
         </>
     )
