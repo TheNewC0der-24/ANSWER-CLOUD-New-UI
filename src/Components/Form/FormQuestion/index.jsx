647040b55865d2ccd2a08c6dcef30d68351b31b5
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-12-12 15:25:14 +0530

    Update UI

--- src/Components/Form/FormQuestion/index.jsx
+++ src/Components/Form/FormQuestion/index.jsx
@@ -113,24 +113,27 @@ const FormQuestion = () => {
     return (
         <>
             <Box p={2}>
+                {/* If all 3 cards */}
                 <Grid container spacing={1}>
-                    <Grid item xs={12} sm={12} md={12} lg={5}>
-                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem" }}>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
+                            <CardContent>
+                                <Typography variant="h4">Description</Typography>
+                                <Typography variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
                             <CardContent>
                                 <Stack p={1} sx={{ display: "block", width: '100%' }}>
                                     <Typography variant="h6" color='primary'>Hint : Speak about your educational background, skills, experience, etc.</Typography>
                                 </Stack>
                             </CardContent>
                         </Card>
-
-                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem" }}>
-                            <CardContent>
-                                <Typography variant="h4">Description</Typography>
-                                <Typography variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
-                            </CardContent>
-                        </Card>
-
-                        <Card sx={{ backgroundColor: "#F7F8F9" }}>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <Card sx={{ backgroundColor: "#F7F8F9", height: "100%" }}>
                             <CardContent>
                                 <Typography variant="h4">Video Context</Typography>
                                 <video
@@ -141,59 +144,92 @@ const FormQuestion = () => {
                             </CardContent>
                         </Card>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={12} lg={7}>
-                        <Card sx={{ backgroundColor: "#F7F8F9" }}>
+                </Grid>
+
+                {/* If any of 2 cards */}
+                {/* <Grid container spacing={1}>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
                             <CardContent>
-                                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                                    <Typography variant="h4">1/4</Typography>
-                                    {/* <Box sx={{ display: "flex", gap: "0.5rem" }}>
-                                        <Typography variant="subtitle1">time remaining for this question</Typography>
-                                        <Typography variant="subtitle1">{minute}:{second}</Typography>
-                                    </Box> */}
-                                </Box>
-
-                                <Typography mb={2} variant="h6">Tell me about yourself ?</Typography>
-                                <Box mb={1} sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <Button my={2} onClick={handleStartRecording} variant="contained" color={isActive ? 'warning' : 'success'} size='small'>{isActive ? "Pause" : "Answer"}</Button>
-                                    {
-                                        isActive && (
-                                            <Box sx={{ display: "flex", gap: "0.5rem" }}>
-                                                <Button onClick={handleRetake} variant="outlined" color='secondary' size='small'>Retake Answer</Button>
-                                                <Button onClick={handleStopRecording} variant="contained" color='secondary' size='small'>Save Answer</Button>
-                                            </Box>
-                                        )
-                                    }
-                                </Box>
-                                <Divider ></Divider>
-                                <Box mt={2} mb={2} sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <Button variant="outlined" size='small'>Back</Button>
-                                    <Button onClick={() => navigate('/form/audio-question')} variant="contained" size='small'>Next</Button>
-                                </Box>
-
-                                {
-                                    !isActive && (
-                                        <Alert severity="info">Answer recording starts after you hit "Answer" button above.</Alert>
-                                    )
-                                }
-                                {
-                                    isActive && (
-                                        <Box>
-                                            {status !== "stopped" ? (
-                                                <Box p={3} sx={{ backgroundColor: "#1976d2" }}>
-                                                    <VideoPreview className={styles.video} stream={previewStream} />
-                                                </Box>
-                                            ) : (
-                                                <Box p={3} sx={{ backgroundColor: "#1976d2" }}>
-                                                    <video className={styles.video} src={mediaBlobUrl} controls />
-                                                </Box>
-                                            )}
-                                        </Box>
-                                    )
-                                }
+                                <Typography variant="h4">Description</Typography>
+                                <Typography variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
-                </Grid>
+                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                        <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
+                            <CardContent>
+                                <Stack p={1} sx={{ display: "block", width: '100%' }}>
+                                    <Typography variant="h6" color='primary'>Hint : Speak about your educational background, skills, experience, etc.</Typography>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid> */}
+
+
+                {/* If only 1 card */}
+                {/* <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
+                    <CardContent>
+                        <Typography variant="h4">Description</Typography>
+                        <Typography variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
+                    </CardContent>
+                </Card> */}
+
+
+
+                <Box mt={2}>
+                    <Card sx={{ backgroundColor: "#F7F8F9" }}>
+                        <CardContent>
+                            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                                <Typography variant="h4">1/4</Typography>
+                                {/* <Box sx={{ display: "flex", gap: "0.5rem" }}>
+                                        <Typography variant="subtitle1">time remaining for this question</Typography>
+                                        <Typography variant="subtitle1">{minute}:{second}</Typography>
+                                    </Box> */}
+                            </Box>
+
+                            <Typography mb={2} variant="h6">Tell me about yourself ?</Typography>
+                            <Box mb={1} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Button my={2} onClick={handleStartRecording} variant="contained" color={isActive ? 'warning' : 'success'} size='small'>{isActive ? "Pause" : "Answer"}</Button>
+                                {
+                                    isActive && (
+                                        <Box sx={{ display: "flex", gap: "0.5rem" }}>
+                                            <Button onClick={handleRetake} variant="outlined" color='secondary' size='small'>Retake Answer</Button>
+                                            <Button onClick={handleStopRecording} variant="contained" color='secondary' size='small'>Save Answer</Button>
+                                        </Box>
+                                    )
+                                }
+                            </Box>
+                            <Divider ></Divider>
+                            <Box mt={2} mb={2} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Button variant="outlined" size='small'>Back</Button>
+                                <Button onClick={() => navigate('/form/audio-question')} variant="contained" size='small'>Next</Button>
+                            </Box>
+
+                            {
+                                !isActive && (
+                                    <Alert severity="info">Answer recording starts after you hit "Answer" button above.</Alert>
+                                )
+                            }
+                            {
+                                isActive && (
+                                    <Box>
+                                        {status !== "stopped" ? (
+                                            <Box p={3} sx={{ backgroundColor: "#1976d2" }}>
+                                                <VideoPreview className={styles.video} stream={previewStream} />
+                                            </Box>
+                                        ) : (
+                                            <Box p={3} sx={{ backgroundColor: "#1976d2" }}>
+                                                <video className={styles.video} src={mediaBlobUrl} controls />
+                                            </Box>
+                                        )}
+                                    </Box>
+                                )
+                            }
+                        </CardContent>
+                    </Card>
+                </Box>
             </Box>
         </>
     )
