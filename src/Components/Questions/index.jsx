992ded5236cc978b248f466125ca6c05931c8c67
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-12-25 21:51:39 +0530

    Improve UI

--- src/Components/Questions/index.jsx
+++ src/Components/Questions/index.jsx
@@ -32,25 +32,29 @@ const Questions = () => {
                     <Grid item xs={12} sm={12} md={4} lg={4}>
                         <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
                             <CardContent>
-                                <Typography variant="h4">Description</Typography>
-                                <Typography variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
+                                <Box p={2} sx={{ bgcolor: "#1976d2" }}>
+                                    <Typography color='white' variant="h4">Description</Typography>
+                                </Box>
+                                <Typography mt={2} variant="subtitle1">This is a video question. You will be asked to record a video of yourself answering the question.</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={12} md={4} lg={4}>
                         <Card sx={{ backgroundColor: "#F7F8F9", marginBottom: "1rem", height: "100%" }}>
                             <CardContent>
-                                <Stack p={1} sx={{ display: "block", width: '100%' }}>
-                                    <Typography variant="h4">Hint</Typography>
-                                    <Typography variant="subtitle1">Hint Speak about your educational background, skills, experience, etc.</Typography>
-                                </Stack>
+                                <Box p={2} sx={{ bgcolor: "#1976d2" }}>
+                                    <Typography color='white' variant="h4">Hint</Typography>
+                                </Box>
+                                <Typography mt={2} variant="subtitle1">Hint Speak about your educational background, skills, experience, etc.</Typography>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={12} md={4} lg={4}>
                         <Card sx={{ backgroundColor: "#F7F8F9", height: "100%" }}>
                             <CardContent>
-                                <Typography variant="h4">Video Context</Typography>
+                                <Box p={2} mb={2} sx={{ bgcolor: "#1976d2" }}>
+                                    <Typography color='white' variant="h4">Video/Audio Context</Typography>
+                                </Box>
                                 <video
                                     src={video}
                                     controls
