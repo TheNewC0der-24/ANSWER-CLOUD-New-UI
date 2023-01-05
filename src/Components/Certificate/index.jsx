bec28672c68c27ea97683f973e0dfabf4613c011
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-05 22:49:05 +0530

    Remove Logo Placeholder

--- src/Components/Certificate/index.jsx
+++ src/Components/Certificate/index.jsx
@@ -30,6 +30,9 @@ const Certificate = () => {
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
                     <Box ref={certificateRef} mt={2} mb={3} p={3} className={styles.certificate}>
                         <Typography mt={3} mb={5} color='primary' align='center' variant='h4'>Company Name</Typography>
+                        {/* <Box>
+                            <img src="" alt="" className="" />
+                        </Box> */}
                         {/* <Typography mt={3} variant='h3'>Certificate of Excellence</Typography> */}
                         {/* <Typography variant='h5' sx={{ fontStyle: "italic" }}>Awarded to</Typography> */}
                         <Typography mt={5} mb={2} color='secondary' align='center' variant='h2'>Candidate Name</Typography>
@@ -46,7 +49,7 @@ const Certificate = () => {
                             </Box>
                             <Box align='end'>
                                 <Typography variant='subtitle1' sx={{ fontStyle: "italic" }}>Powered by</Typography>
-                                <img src={brandLogo} alt="brand" style={{ width: '70%' }} />
+                                <img src={brandLogo} alt="brand" style={{ width: '70%', transform: 'translateX(6px)' }} />
                             </Box>
                         </Box>
                         <Typography color='error' mt={5} variant='body1' sx={{ fontStyle: "italic" }} align='center'>
